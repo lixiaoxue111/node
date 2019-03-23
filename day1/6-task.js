93009b3d5da9efd65a9e7f63ae1cd5da10f42d3f
newlls <523786175@qq.com>
2019-03-23 12:20:40 +0800

    练习

---
+++ day1/6-task.js
@@ -0,0 +1,20 @@
+console.log('1');
+
+setTimeout(function(){ //宏任务
+    console.log('2');
+    process.nextTick(function () {
+        console.log('3');
+    });
+    new Promise(function (resolve) {
+        console.log('4');
+        resolve();
+    }).then(function () {
+        console.log('5');
+    })
+});
+
+process.nextTick(function () { //微任务
+   console.log('6');
+});
+
+// 1  6  2  4  3  5
