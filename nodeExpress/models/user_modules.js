e3c3f2b879d201f6a9fa29ff4a97507a63b12ea2
newlls <523786175@qq.com>
2019-05-02 16:45:30 +0800

    练习

--- nodeExpress/models/user_modules.js
+++ nodeExpress/models/user_modules.js
@@ -1,13 +1,14 @@
+/*
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'me',
-    password : 'secret',
-    database : 'my_db'
+    user     : 'root',
+    password : '',
+    database : 'db'
 });
 
 exports.sel_data=function(callback){
-    connection.connect();
+    /!*connection.connect();
 
     connection.query('select * from user', function (error, results, fields) {
         if (error) throw error;
@@ -15,5 +16,25 @@ exports.sel_data=function(callback){
         //console.log(results);
         callback(results);
         connection.end();
-    });
-}
+    });*!/
+    var sql="select * from user "
+};*/
+var db=require('./db.js');
+
+exports.reg_data=function (name,pass,callback) {
+    var sql="insert into t_users(USER_ID,ACCOUNT,PASSWORD)";
+    db.query(sql,[name,pass],callback);
+};
+
+exports.sel_data=function (name,pass,callback) {
+   // var sql="insert into user(uid,name,pass) values(null,?,?)";
+    //var sql="select * from t_users where ACCOUNT=? and PASSWORD=?";
+    var sql="select * from t_user";
+    db.query(sql,[name,pass],callback);
+};
+
+exports.c_name=function (name,callback) {
+    var sql="select * from user where name=? pass=?";
+    db.query(sql,[name,pass],callback);
+    
+};
